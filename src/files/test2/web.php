51ba2fe99fc2a1766085dee62c88a5bc6e817bac
kaprisa57@gmail.com <kaprisa57@gmail.com>
2018-05-27 10:50:52 +0800

    c

--- src/files/test2/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::get('/', 'WelcomeController@landing');
-//Route::get('/academy', 'WelcomeController@index');
-Route::get('/{a}', 'WelcomeController@index');
-Route::get('/{a}/{b}', 'WelcomeController@index');
-Route::get('/{a}/{b}/{c}', 'WelcomeController@index');
-Route::get('/{a}/{b}/{c}/{d}', 'WelcomeController@index');
